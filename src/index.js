a2f940c76bd8d8e97727216879ef3804a14efc09
Dylan Frankland <dylan@frankland.io>
2017-12-16 15:54:43 -0800

    Switch object literal to Map

--- src/index.js
+++ src/index.js
@@ -3,44 +3,41 @@
 import { wrap } from 'kaffeerost';
 import zopfliWasmModule from './zopfli/Cargo.toml';
 
-export const FORMAT_DEFLATE = 'deflate';
-export const FORMAT_GZIP = 'gzip';
-export const FORMAT_ZLIB = 'zlib';
+export const FORMAT_DEFLATE = {};
+export const FORMAT_GZIP = {};
+export const FORMAT_ZLIB = {};
 
-const formats = {
-  [FORMAT_DEFLATE]: true,
-  [FORMAT_GZIP]: true,
-  [FORMAT_ZLIB]: true,
-};
+const formats = new Map([
+  [FORMAT_DEFLATE, 'deflate'],
+  [FORMAT_GZIP, 'gzip'],
+  [FORMAT_ZLIB, 'zlib'],
+]);
 
-export const zopfli = (format) => {
-  if (!formats[format]) throw new Error('format is invalid');
+export const zopfli = async (format, data) => {
+  if (!formats.has(format)) throw new Error('format is invalid');
+  if (!(data instanceof Uint8Array)) throw new Error('data must be a Uint8Array');
 
-  return async (data) => {
-    if (!(data instanceof Uint8Array)) throw new Error('data must be a Uint8Array');
-
-    const {
-      instance: {
-        exports,
-      } = {},
-    } = await zopfliWasmModule({
-      env: {
-        memory: new WebAssembly.Memory({ initial: 4096, limit: 4096 }),
-        log: x => Math.log(x),
-      },
-    });
-
-    const zopfliFormat = wrap(
+  const {
+    instance: {
       exports,
-      format,
-      ['&[u8]'],
-      'Vec<u8>',
-    );
+    } = {},
+  } = await zopfliWasmModule({
+    env: {
+      memory: new WebAssembly.Memory({ initial: 4096, limit: 4096 }),
+      log: x => Math.log(x),
+    },
+  });
 
-    return zopfliFormat(data);
-  };
+  const zopfliFormat = wrap(
+    exports,
+    formats.get(format),
+    ['&[u8]'],
+    'Vec<u8>',
+  );
+
+  return zopfliFormat(data);
 };
 
-export const deflate = zopfli(FORMAT_DEFLATE);
-export const gzip = zopfli(FORMAT_GZIP);
-export const zlib = zopfli(FORMAT_ZLIB);
+export const deflate = (...args) => zopfli(FORMAT_DEFLATE, ...args);
+export const gzip = (...args) => zopfli(FORMAT_GZIP, ...args);
+export const zlib = (...args) => zopfli(FORMAT_ZLIB, ...args);
