1a470d27338d255c10c13e61698666dd1c7a0c12
Dylan Frankland <dylan@frankland.io>
2017-12-16 15:47:41 -0800

    Add lint config and tests

--- src/__tests__/wasm-zopfli.spec.js
+++ src/__tests__/wasm-zopfli.spec.js
@@ -1,20 +1,75 @@
-/* eslint-disable no-console */
+import { promisify } from 'util';
+import { inflateRaw, gunzip, inflate } from 'zlib';
+import { readFileSync } from 'fs';
+import { runInNewContext } from 'vm';
 
-const { gunzipSync } = require('zlib');
-const zopfli = require('../../dist');
+jest.setTimeout(15000);
 
-const logData = [];
-
-const INPUT = Buffer.from('blah', 'utf8');
-
-const OUTPUT = (data) => {
-  console.log('INPUT:', INPUT.toString('utf8'));
-  console.log('OUTPUT COMPRESSED:', Buffer.from(data).toString('utf8'));
-  console.log('OUTPUT DECOMPRESSED:', gunzipSync(data).toString('utf8'));
-  console.log('LOG:', Buffer.from(Uint8Array.from(logData)).toString('utf8'));
+// This fixes a problem with Jest, which makes it freeze when importing
+// `wasm-zopfli` for some reason.
+const wasmZopfli = readFileSync(require.resolve('../../'), { encoding: 'utf8' });
+const sandbox = {
+  exports: {},
+  require,
+  Buffer,
+  Uint8Array,
 };
+runInNewContext(wasmZopfli, sandbox);
+const {
+  exports: {
+    deflate,
+    gzip,
+    zlib,
+    zopfli,
+  },
+} = sandbox;
 
-zopfli
-  .gzip(INPUT, log => logData.push(log))
-  .then(OUTPUT)
-  .catch(console.error);
+describe('zlib', () => {
+  it('can deflate (deflate raw)', async () => {
+    const input = 'blah';
+
+    const compressed = await deflate(Buffer.from(input, 'utf8'));
+    const decompressed = await promisify(inflateRaw)(compressed);
+    const result = decompressed.toString('utf8');
+
+    expect(result).toEqual(input);
+  });
+
+  it('can gzip', async () => {
+    const input = 'blah';
+
+    const compressed = await gzip(Buffer.from(input, 'utf8'));
+    const decompressed = await promisify(gunzip)(compressed);
+    const result = decompressed.toString('utf8');
+
+    expect(result).toEqual(input);
+  });
+
+  it('can zlib (deflate)', async () => {
+    const input = 'blah';
+
+    const compressed = await zlib(Buffer.from(input, 'utf8'));
+    const decompressed = await promisify(inflate)(compressed);
+    const result = decompressed.toString('utf8');
+
+    expect(result).toEqual(input);
+  });
+
+  it('checks for `Uint8Array` input', async () => {
+    try {
+      await zlib('blah');
+    } catch (err) {
+      return expect((
+        () => {
+          throw err;
+        }
+      )).toThrow('data must be a Uint8Array');
+    }
+
+    throw new Error('test failed');
+  });
+
+  it('checks for proper format given to `zopfli` function', () => {
+    expect(() => zopfli('blah')).toThrow('format is invalid');
+  });
+});
